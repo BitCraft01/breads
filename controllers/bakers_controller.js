d217cde759e46f28329fcaafd7ed350cae2c0aa0
Bachir <bachirmdicko@gmail.com>
2022-08-29 17:32:50 -0400

    Improvement

--- controllers/bakers_controller.js
+++ controllers/bakers_controller.js
@@ -32,6 +32,15 @@ baker.get('/:id', (req, res) => {
             })
     })
 
+// delete
+baker.delete('/:id', (req, res) => {
+    Baker.findByIdAndDelete(req.params.id) 
+      .then(deletedBaker => { 
+        res.status(303).redirect('/breads')
+      })
+})
+
+
 
 // export
 module.exports = baker                    
